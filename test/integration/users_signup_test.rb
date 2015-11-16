d8c1dd18cd9730a8ed007057fcfee65ab0e02248
David Stinnette <dastinnette@gmail.com>
2015-11-16 15:31:13 -0700

    Tested invalid signup process

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid signup info" do
+    get signup_path
+    assert_no_difference "User.count" do
+      post users_path, user: { name: "",
+                               email: "user@invalid",
+                               password: "abc",
+                               password_confirmation: "def" }
+    end
+    assert_template 'users/new'
+  end
 end
